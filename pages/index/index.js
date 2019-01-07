be6efbdf7ee57ba0fe8b45afd37ec766ce3a64e9
huqi <huqi1008301@163.com>
2019-01-07 17:59:36 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+Page({
+  onLoad(query) {
+    // 页面加载
+    console.info(`Page onLoad with query: ${JSON.stringify(query)}`);
+  },
+  onReady() {
+    // 页面加载完成
+  },
+  onShow() {
+    // 页面显示
+  },
+  onHide() {
+    // 页面隐藏
+  },
+  onUnload() {
+    // 页面被关闭
+  },
+  onTitleClick() {
+    // 标题被点击
+  },
+  onPullDownRefresh() {
+    // 页面被下拉
+  },
+  onReachBottom() {
+    // 页面被拉到底部
+  },
+  onShareAppMessage() {
+    // 返回自定义分享信息
+    return {
+      title: '工业设计',
+      desc: '广州亓行智能科技,打造您的智能家居！',
+      path: 'pages/index/index',
+    };
+  },
+    makePhoneCall() {
+    my.makePhoneCall({ number: '02037268390' });
+  }
+});
